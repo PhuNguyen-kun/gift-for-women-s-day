13270d55fe1db6696748888c519b03ea1cafb9bf
Phu Nguyen <nguyenphu43kun@gmaill.com>
2024-10-19 23:35:34 +0700

    fix route

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/cac-anh-trai-Markat-sieu-dep-zai',
+      path: '/',
       name: 'welcome',
       component: () => import('@/views/welcome.vue'),
     },
